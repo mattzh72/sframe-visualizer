16adb1b3dbcf83651fc6783c973c0ab62258f251
Matt Zhou <mattzh1314@berkeley.edu>
2019-06-29 15:04:13 -0700

    Updated configs file to YAML format. Added support for processing multiple sframes at once.

---
+++ visualize.py
@@ -0,0 +1,32 @@
+import turicreate as tc
+import os
+import yaml
+from tqdm import tqdm
+
+from utils.parse import extract_imgs_from_sframe, compile_video
+
+# LOAD IN CONFIGURATIONS
+with open("configs.yaml", 'r') as stream:
+	try:
+		configs = yaml.safe_load(stream)
+	except yaml.YAMLError as exc:
+		print(exc)
+
+sframes = [file for file in os.listdir(configs['sframe_dir']) if file.endswith(".sframe")]
+pbar = tqdm(sframes)
+for sframe in pbar:
+	frames = extract_imgs_from_sframe(
+		os.path.join(configs['sframe_dir'], sframe), 
+		target_label=configs['target_label'],
+		buffer=configs['buffer'],
+		draw_center=configs['draw_centers'],
+		draw_center_line=configs['draw_contrail'],
+		draw_boundings=configs['draw_boundings'], 
+		draw_masks=configs['draw_masks'],
+		draw_frame_num=configs['draw_frame_num'], 
+		annotations_col=configs['annotations_col'], 
+		image_col=configs['image_col'], 
+		masks_col=configs['masks_col'])
+
+	# Write to disk
+	compile_video(frames, target=os.path.join(configs['target_dir'], sframe.split('.')[-2] + '.mp4'), fps=configs['fps'])
